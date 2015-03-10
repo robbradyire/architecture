720334a3f29b1b92757917fa11e090494f8a4d51
Rob <robbradyire@gmail.com>
2015-03-10 15:33:43 +0000

    Initial commit.

---
+++ lab2/src/function_unit.vhd
@@ -0,0 +1,22 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity function_unit is
+port (
+	data_A, data_B : in std_logic_vector(15 downto 0);
+	operation_sel : in std_logic_vector(4 downto 0);
+	data_out : out std_logic_vector(15 downto 0);
+	overflow, carry_out, negative, zero : out std_logic
+	);
+end function_unit;
+
+architecture Behavioral of function_unit is
+begin
+	data_out <= data_A after 20 ns;
+	carry_out <= carry_in after 20 ns;
+	overflow <= '0' after 20 ns;
+	negative <= '0' after 20 ns;
+	zero <= '0' after 20 ns;
+end Behavioral;
